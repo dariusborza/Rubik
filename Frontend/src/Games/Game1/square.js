2d22b54dda68bc07aacb2a21baed6f361e23057f
Borza Darius <102525392+dariusborza@users.noreply.github.com>
2024-07-24 21:52:15 +0300

    Delete Frontend directory

--- Frontend/src/Games/Game1/square.js
+++
@@ -1,9 +0,0 @@
-function Square(props) {
-    return (
-      <div className={'square'} {...props}>
-        {props.x ? 'x' : (props.o ? 'o' : '')}
-        </div>
-    );
-  }
-  
-  export default Square;
